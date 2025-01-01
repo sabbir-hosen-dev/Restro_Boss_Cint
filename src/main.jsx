f667a395ae75d9a59bc250ee8add85ff7dedfc2a
sabbir-hosen-dev <tssabbirhosen@gmail.com>
2025-01-01 13:39:58 +0600

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import {
+  RouterProvider,
+} from "react-router-dom";
+import { router } from './Routes/Routes';
+import { HelmetProvider } from 'react-helmet-async';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HelmetProvider>
+      <div className='max-w-screen-xl mx-auto'>
+        <RouterProvider router={router} />
+      </div>
+    </HelmetProvider>
+  </React.StrictMode>,
 )
